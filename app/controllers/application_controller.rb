8391ec9c59590c79a25d3e82ac39e637c757a97f
David Hawdale <david.hawdale@gmail.com>
2014-01-04 22:36:44 +0000

    Revert "bill"
    This reverts commit 6bd85b605782e6a5d351be27a105761c5753bc80.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception. bnbghgh
+  # Prevent CSRF attacks by raising an exception. bnb
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
